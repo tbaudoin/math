fdd7d6328555604f39308847c92c4a09975d39ba
Thierry Baudoin <baudoin.thierry@gmail.com>
2021-09-30 13:48:44 +0200

    basic example of an updater

---
+++ manim/learning/readme.md
@@ -0,0 +1,4 @@
+# Découverte de Manim
+[Tutoriel](https://www.youtube.com/watch?v=MOv6yN7b2aI&list=PLWOlLjdyZm2NQD1YZmEPB0dwbd0yKINAT)
+
+[Documentation](https://docs.manim.community/en/stable/)
---
+++ manim/learning/tuto1/tuto1.py
@@ -0,0 +1,32 @@
+import math
+from manim import *
+
+
+class RectangleWithFraction(Scene):
+    """ Moving rectangle with a fraction in his center."""
+    def construct(self):
+        # create a rectangle
+        rectangle = Rectangle(
+            color=WHITE,
+            width=4.5,
+            height=2,
+        )
+        # move the rectangle
+        rectangle.shift(2*UP + 4*LEFT)
+        # create a math text
+        mathtext = MathTex(
+            '\\frac{3}{2}'
+        )
+        # move the frac in the center of the rectangle
+        mathtext.move_to(rectangle)
+        # add an updater, if the rectangle move the fraction get stick in his
+        # center
+        mathtext.add_updater(lambda x : x.move_to(rectangle))
+        # animation
+        self.play(FadeIn(rectangle))
+        self.play(Write(mathtext))
+        self.play(rectangle.animate.shift(4*DOWN+2*RIGHT))
+        self.wait()
+        # remove updater
+        mathtext.clear_updaters()
+        self.play(rectangle.animate.shift(4*UP + 2*LEFT))
